b875233bb0eead1ae4054619789ddcb4fba2b64f
Gent Gashi <gentgaashi@gmail.com>
2016-06-14 18:45:02 +0200

    removed dist

--- dist/util/errors/Errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var NotFoundError = (function () {
-    function NotFoundError(message, status) {
-        this.message = message;
-        this.status = status;
-    }
-    return NotFoundError;
-}());
-exports.NotFoundError = NotFoundError;
--- dist/util/helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var uuid = require('node-uuid');
-function generateGuid() {
-    var timeString = uuid.v4();
-    return timeString.replace(/\./g, '');
-}
-exports.generateGuid = generateGuid;
